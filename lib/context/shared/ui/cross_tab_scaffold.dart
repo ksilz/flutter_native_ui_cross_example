bd135e1cd59e3d31e8d67425a3614c7cb89580fd
Karsten Silz <karsten.silz@outlook.com>
2022-03-05 22:11:54 +0000

    Windows version
    Initial implementation.

--- lib/context/shared/ui/cross_tab_scaffold.dart
+++ lib/context/shared/ui/cross_tab_scaffold.dart
@@ -1,11 +1,13 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:macos_ui/macos_ui.dart';
+import 'package:window_manager/window_manager.dart';
 
 import '../domain/s_enums.dart';
 import '../service/s_device_service.dart';
-import 'cross_coming_soon.dart';
 import 'cross_nav_bar_item.dart';
 
 class CrossTabScaffold extends StatefulWidget {
@@ -93,7 +95,35 @@ class _CrossTabScaffoldState extends State<CrossTabScaffold> {
         break;
 
       case SPlatform.windows:
-        feedback = const CrossComingSoon(label: 'CrossTabScaffold');
+        feedback = NavigationView(
+          appBar: NavigationAppBar(
+            title: () {
+              if (kIsWeb) return Text(_calculateScreenTitle());
+              return DragToMoveArea(
+                child: Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text(_calculateScreenTitle()),
+                ),
+              );
+            }(),
+          ),
+          pane: NavigationPane(
+            selected: _selectedIndex,
+            onChanged: (index) => _updateIndex(index),
+            size: const NavigationPaneSize(
+              openMinWidth: 200,
+              openMaxWidth: 250,
+            ),
+            items: widget.items
+                .map(
+                  (anItem) => PaneItem(
+                    icon: Icon(anItem.icon),
+                    title: Text(anItem.label),
+                  ),
+                )
+                .toList(),
+          ),
+        );
         break;
 
       case SPlatform.linux:
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import native_context_menu
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   NativeContextMenuPlugin.register(with: registry.registrar(forPlugin: "NativeContextMenuPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
